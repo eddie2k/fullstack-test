7a02ff368598e7efa8bc02cf76fdf08d53646244
eddie2k <jesus_eme_ge@hotmail.com>
2019-09-26 00:32:19 +0200

    Handle network error

--- react-frontend/src/components/MyComponent.jsx
+++ react-frontend/src/components/MyComponent.jsx
@@ -6,17 +6,27 @@ import React, {useEffect, useState} from "react"
  * @constructor
  */
 const MyComponent = () => {
-    const [name, setName] = useState("Luke");
+    const [name, setName] = useState("Unknown");
+    const [networkError, setNetworkError] = useState(false);
     useEffect(() => {
         fetch('http://localhost:8080/api/v1/sw/character')
             .then(res => res.json())
             .then((data) => {
                 setName(data.name)
+                setNetworkError(false)
             })
+            .catch((error) => {
+                setNetworkError(true)
+            });
     }, []);
 
     return <div>
-        Name: {name}
+        <div>
+            Name: {name}
+        </div>
+        <div className={!networkError ? 'hidden' : ''}>
+            Status: Can't connect to the server!
+        </div>
     </div>
 };
 
