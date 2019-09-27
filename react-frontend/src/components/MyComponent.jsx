4d78b573473ea8fb46b0421c90ed45dae71c568f
eddie2k <jesus_eme_ge@hotmail.com>
2019-09-27 17:16:43 +0200

    Frontend error/loading handling

--- react-frontend/src/components/MyComponent.jsx
+++ react-frontend/src/components/MyComponent.jsx
@@ -7,11 +7,15 @@ import React, {useEffect, useState} from "react"
  */
 const MyComponent = () => {
     const [name, setName] = useState("Unknown");
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         fetch('http://localhost:8080/api/v1/sw/character')
             .then(res => res.json())
             .then((data) => {
+                setLoading(false);
                 if (data.integrationOk) {
                     setName(data.starWarsCharacter.name);
                     setError(false);
@@ -20,16 +24,20 @@ const MyComponent = () => {
                 }
             })
             .catch((error) => {
+                setLoading(false);
                 setError(true);
             });
     }, []);
 
     return <div>
-        <div>
+        <div className={loading || error ? 'hidden' : ''}>
             Name: {name}
         </div>
+        <div className={!loading ? 'hidden' : ''}>
+            Status: Loading...
+        </div>
         <div className={!error ? 'hidden' : ''}>
-            Status: Can't fetch character information!
+            Error: Can't fetch character information!
         </div>
     </div>
 };
